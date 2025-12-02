45fdf180487d138a141f86b18d906370f96f7c4c
Araxeus <78568641+Araxeus@users.noreply.github.com>
2025-12-03 00:33:41 +0200

    refactor login from github.ts to auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,60 @@
 import { Entry } from '@napi-rs/keyring';
+import { createOAuthDeviceAuth } from '@octokit/auth-oauth-device';
+import open from 'open';
+import { assert, error, success } from './utils.js';
 
 const keyring = new Entry('vendorfiles-cli', 'github_token');
 
-let password = process.env.GITHUB_TOKEN || keyring.getPassword() || undefined;
+export const token =
+    process.env.GITHUB_TOKEN || keyring.getPassword() || undefined;
 
-export const get = () => password;
+export async function login(token?: string) {
+    if (token) {
+        const res = await fetch('https://api.github.com', {
+            cache: 'no-store',
+            method: 'HEAD',
+            headers: {
+                Authorization: `bearer ${token}`,
+            },
+        });
 
-export const set = (newToken: string) => {
-    keyring.setPassword(newToken);
-    password = newToken;
-};
+        assert(res.status !== 401, 'Invalid token');
+        assert(res.status !== 403, 'Token is rate limited');
+        assert(res.ok, 'Something went wrong, try again later');
+        keyring.setPassword(token);
+        success('Token saved successfully');
+        return;
+    }
+    try {
+        const auth = createOAuthDeviceAuth({
+            clientType: 'oauth-app',
+            clientId: '39d3104ecbbfd876dfa5',
+            scopes: [],
+            async onVerification(verification) {
+                console.log(
+                    `First, copy your one-time code: ${verification.user_code}`,
+                );
+                console.log(
+                    'Then press [Enter] to continue in your web browser',
+                );
+                await new Promise(resolve => {
+                    process.stdin.once('data', resolve);
+                });
+                console.log('Opening your web browser...');
+                await open(verification.verification_uri);
+            },
+        });
+
+        const tokenAuthentication = await auth({
+            type: 'oauth',
+        });
+
+        keyring.setPassword(tokenAuthentication.token);
+
+        success('Logged in successfully');
+    } catch (e) {
+        error(e as string);
+    } finally {
+        process.exit(0);
+    }
+}
